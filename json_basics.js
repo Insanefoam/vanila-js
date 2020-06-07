3bc9d40c50eb02421eb110a85f4eb136470c8514
Insanefoam <spartaq123456789@gmail.com>
2020-06-07 18:48:33 +0600

    json

--- json_basics.js
+++ json_basics.js
@@ -12,4 +12,33 @@ let student = {
 
 //JSON ignore functions, Symbols and undefiend values
 let json = JSON.stringify(student);
-console.log(json)
+//console.log(json)
+
+//Result of parse JSON
+let obj = JSON.parse(json)
+//console.log(obj)
+
+//Recursive structure down here
+let room = {
+    number: 25
+}
+
+let meetup = {
+    title: 'Programming discussion',
+    participations: [{ name: 'Oleg' }, { name: 'Inga' }],
+    place: room
+}
+
+room.occupiedBy = meetup
+//Fix with Replacer
+//And space parameter example
+let string = JSON.stringify(meetup, (key, value) => key === 'occupiedBy' ? undefined : value, 50)
+//console.log(string)
+
+//Change to JSON function
+room.toJSON = function () {
+    return this.number
+}
+
+string = JSON.stringify(meetup, (key, value) => key === 'occupiedBy' ? undefined : value)
+console.log(string)
